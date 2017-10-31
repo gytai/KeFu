cbf7d2689518f5b8d787eab9f921fd658513beb5
邰广银 <273650669@qq.com>
2017-10-31 19:17:10 +0800

    修改路由地址 客服端-> '/' 客户端-> '/client'

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('./client/index');
+  res.render('./server/index');
 });
 
 router.get('/admin', function(req, res, next) {
   res.render('./server/index');
+});
+
+router.get('/client', function(req, res, next) {
+    res.render('./client/index');
 });
 
 router.get('/admin/users', function(req, res, next) {
